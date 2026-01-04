2e633d6b4a825d581028e3ce2fa70c3f5118d6b7
Patrick-Poultry <musazapatrick369@gmail.com>
2026-01-04 13:03:47 +0200

    .NET MAUI Setup

---
+++ Desktop/LaundromatManagementSystem/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LaundromatManagementSystem;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
