954230ef1da1a9b324cd9dbb82ffe7e56b3c3273
Patrick-Poultry <musazapatrick369@gmail.com>
2026-01-09 15:24:10 +0200

    Local db Services Addons

---
+++ Desktop/LaundromatManagementSystem/Models/ServiceAddon.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaundromatManagementSystem.Models
+{
+    public class ServiceAddon : BaseEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+        
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+        
+        [Required]
+        public int ServiceId { get; set; }
+        
+        // Navigation property
+        [ForeignKey("ServiceId")]
+        public virtual Service Service { get; set; } = null!;
+    }
+}
