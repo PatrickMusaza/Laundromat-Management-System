dda2b045414a728fc2a77b3f39d469ddf88f5192
Patrick-Poultry <musazapatrick369@gmail.com>
2026-01-23 22:29:02 +0200

    Windows FullScreen, no title bar , no buttons

--- Desktop/LaundromatManagementSystem/AppShell.xaml.cs
+++ Desktop/LaundromatManagementSystem/AppShell.xaml.cs
@@ -1,9 +1,32 @@
-﻿namespace LaundromatManagementSystem;
+﻿#if WINDOWS
+using Microsoft.UI.Windowing;
+#endif
+
+namespace LaundromatManagementSystem;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public AppShell()
+    {
+        InitializeComponent();
+
+        // Subscribe to Loaded event correctly
+        Loaded += OnLoaded;
+    }
+
+#if WINDOWS
+    private void OnLoaded(object sender, EventArgs e)
+    {
+        var window = this.Window;
+        if (window?.Handler?.PlatformView is Microsoft.UI.Xaml.Window nativeWindow)
+        {
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+
+            // Fullscreen, no title bar, no buttons
+            appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+        }
+    }
+#endif
 }
